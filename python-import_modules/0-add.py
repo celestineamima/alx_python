9794ddf96bef565947d6dee840fb61230d10b2f7
celestineamima <celestineamima@gmail.com>
2023-07-27 11:47:32 +0300

    Importing modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
-from add__0.py import add (a, b)
+from add_0 import add
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-print('{} + {} = {}'.format(a, b, b + c))
-
-a = 1
-b = 2
-
-return (a + b)
-  
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
+    
+    
--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,9 +1,13 @@
-from sys import argv
+import sys
+
+def print_arguments():
+    num_arguments = len(sys.argv) - 1
+    arguments_list = sys.argv[1:]
+
+    print(f"Number of arguments: {num_arguments}")
+    print("List of arguments:")
+    for idx, arg in enumerate(arguments_list, 1):
+        print(f"{idx}. {arg}")
+
 if __name__ == "__main__":
-    def print_arguments(args):
-        number = len(argv)
-        if number==0:
-            print("{} argument")
-        elif number==1:
-            print("{} arguments.")
-            print("{}: {}".format(number, args))
+    print_arguments()
