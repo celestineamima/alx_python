244053b3cdcc88350a22260259ad510ebc7bda64
celestineamima <celestineamima@gmail.com>
2023-07-27 22:01:10 +0300

    More python data structures

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,15 @@
-def square_matrix_simple(matrix = []):
-    return [[element**2 for element in row] for row in matrix]
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = [] 
+    for row in matrix:
+      new_row = [] 
+      for number in row:
+         new_number = number ** 2
+         new_row.append(new_number) #new results
+      new_matrix.append(new_row)
+    return new_matrix
+def display_new_matrix(matrix=[]):
+    for row in matrix:
+      print(", ".join(str(number)for number in row))
+      
+      
